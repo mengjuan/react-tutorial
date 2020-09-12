202f5006c35962730d13d2731c91ef42e46ab8c1
Mona Hu <yezimengjuan@126.com>
2020-09-12 17:15:23 +0800

    Updates

---
+++ precache-manifest.bbbbadc1af9e4417dc3e758c646e2318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7622ac6fae28ad009ab604a1886055",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "aa275410bab650706ebe",
+    "url": "/react-tutorial/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa275410bab650706ebe",
+    "url": "/react-tutorial/static/js/main.dcc94714.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
